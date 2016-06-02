bb870228fa560d7bdff19e64e34d4a45c50e81dc
Juan Pablo Serra <juanpabloserradanke@gmail.com>
2016-06-02 08:29:37 -0400

    starting params

--- Clami.php
+++ Clami.php
@@ -10,8 +10,15 @@ use yii\base\InvalidConfigException;
  * @author Juan Pablo Sera
  */
 class Clami extends Component{
-	//put your code here
-	 public function init() {
-        
+
+	/**
+     * @var string
+     */
+    public $token;
+
+	public function init() {
+        if (empty($this->token)) {
+            throw new InvalidConfigException('You must set token to authenticate.');
+        }return true;
     }
 }
