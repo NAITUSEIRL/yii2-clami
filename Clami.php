e2f508fe0aed914ef3d4e46265c5e91f90581113
jotape <jotape@NoTeBurles>
2017-05-23 07:45:25 -0400

    ultimos ajustes para produccion de factura electronica para ctm group

--- Clami.php
+++ Clami.php
@@ -171,7 +171,7 @@ class Clami extends Component {
     }else{
         //RutEnvia se pone a mano, es el due;o del certificado para firmar
         $phpData = Json::decode($this->jsonData);
-        $data['Caratula']["RutEnvia"] = $this->testData['RutEnvia'];
+        $phpData['Caratula']["RutEnvia"] = $this->perfilCargado['RutEnvia'];
         $this->jsonData = Json::encode($phpData, $this->jsonEncodeOption);
         \Yii::trace('Json de envio final produccion:' . $this->jsonData, 'Clami' . __METHOD__);
     }
